2d718bf7352d5766d5d5df00469c41d246b1aa0c
Rodrigo Luz <rodrigodsluz@gmail.com>
2022-11-01 14:52:45 -0300

    style: styling and theming

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -12,6 +12,7 @@ class CategoriesScreen extends StatelessWidget {
           title: const Text('Meals App'),
         ),
         body: GridView(
+          padding: const EdgeInsets.all(25),
           children: DUMMY_CATEGORIES
               .map(
                 (catData) => CategoryItem(
